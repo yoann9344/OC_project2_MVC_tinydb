a4b873edc9a4b3e7d16e83ff486b7377b6878ccd
Yoann <yoann9344@gmail.com>
2021-07-23 13:30:32 +0200

    improve shorcuts controller

--- chess_tournament/controllers/loop.py
+++ chess_tournament/controllers/loop.py
@@ -1,17 +1,21 @@
 import copy
+import inspect
 import operator
+import re
 import time
 from abc import ABC, abstractmethod
-from typing import Type
+from typing import Type, Callable
 
 import rich
 from rich.align import Align
+from rich.columns import Columns
 from rich.console import Console, ConsoleOptions, RenderResult
 from rich.live import Live
 from rich.layout import Layout
 from rich.panel import Panel
 from rich.pretty import Pretty
 from rich.prompt import Prompt
+from rich.table import Table
 
 from chess_tournament import models
 from chess_tournament.models.test import Player
@@ -42,6 +46,7 @@ class Browser():
         self._check_index_value()
         # self._page.on_page_change()
         self._page = self.history[self.index]
+        self._page.focus = self._page.focus
         self.need_update = True
 
     def go_to(self, page: 'Page'):
@@ -58,6 +63,9 @@ class Browser():
             self._move()
 
     def go_backward(self):
+        '''Go to the previous page visited
+        shortcut_name = Page précédente
+        '''
         self._check_index_value()
         if self.index == 0:
             return
@@ -66,6 +74,9 @@ class Browser():
         self._move()
 
     def go_foreward(self):
+        '''Go to the next page visited
+        shortcut_name = Page suivante
+        '''
         self._check_index_value()
         if self.index == len(self.history) - 1:
             return
@@ -77,6 +88,9 @@ class Browser():
 class LayoutController(ABC):
     border_style = 'blue'
 
+    def __init__(self, page=None, *args, **kwargs):
+        self.page = page
+
     @abstractmethod
     def update(self, layout: Layout):
         pass
@@ -87,7 +101,8 @@ class LayoutController(ABC):
 
 
 class RowLayoutController(LayoutController):
-    def __init__(self, row: models.Model = None):
+    def __init__(self, row: models.Model = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.row = row
 
     def update(self, layout: Layout):
@@ -104,26 +119,28 @@ class RowLayoutController(LayoutController):
 
 class TableLayoutController(LayoutController):
     def __init__(
-            self,
-            page: 'Page',
-            model: models.Model,
-            headers: list[str] = None,
-            shortcuts: dict = {},
-            info_layout_controller: 'RowLayoutController' = None,
+        self,
+        model: models.Model,
+        headers: list[str] = None,
+        shortcuts: dict = {},
+        info_layout_controller: 'RowLayoutController' = None,
+        *args,
+        **kwargs,
     ):
-        # super().__init__()
-        self.page = page
+        super().__init__(*args, **kwargs)
         self.info_layout_controller = info_layout_controller
         self.shortcuts = {
             'k': self.up,
             'j': self.down,
             't': self.sort,
+            'T': self.reverse_sort,
         }
         self.index = -1
         self.model = model
         self.headers = headers or ['id'] + list(self.model._fields.keys())
         self.sort_by = self.headers[0]
         self.sort_index = 0
+        self.sort_reversed = False
         self.data: list[models.Model] = self.model.all()
         self.table = TableView(
             self.headers,
@@ -140,7 +157,10 @@ class TableLayoutController(LayoutController):
         else:
             selection = None
         self.data: list[models.Model] = self.model.all()
-        self.data.sort(key=operator.attrgetter(self.sort_by))
+        self.data.sort(
+            key=operator.attrgetter(self.sort_by),
+            reverse=self.sort_reversed,
+        )
         if current_row_id is not None:
             self.index = [x.id for x in self.data].index(current_row_id)
             selection = self.index
@@ -155,12 +175,22 @@ class TableLayoutController(LayoutController):
         layout.update(self.table)
 
     def sort(self):
+        '''Sort row by next column
+        shortcut_name = Trier la table
+        '''
         index = self.sort_index + 1
         if index >= len(self.headers):
             self.sort_index = 0
         else:
             self.sort_index = index
         self.sort_by = self.headers[self.sort_index]
+        self.page.update_by_controller(self)
+
+    def reverse_sort(self):
+        '''Reverse the current order
+        shortcut_name = Inverser le tri
+        '''
+        self.sort_reversed ^= True
         self.page.update_by_controller(self)
 
     def _move(self, increment):
@@ -184,17 +214,30 @@ class TableLayoutController(LayoutController):
             self.page.update_by_controller(lc)
 
     def up(self):
+        '''Select upper table row
+        shortcut_name = Sélectionner la ligne supérieure
+        '''
         self._move(1)
 
     def down(self):
+        '''Select upper table row
+        shortcut_name = Sélectionner la ligne inférieure
+        '''
         self._move(-1)
 
 
 class ShortcutsLayoutController(LayoutController):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, shortcuts_global: dict[str, Callable], *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._codes = []
+        self.current_key = None
+        self._shortcuts = {}
+        self.shortcuts_global = shortcuts_global
+        self.keys = list(
+            self._shortcuts.keys() | self.shortcuts_global.keys()
+        )
         self.align = Align.center('', vertical='middle')
+        self.update_table()
         self.panel = Panel(
             self.align,
             style='',
@@ -203,7 +246,41 @@ class ShortcutsLayoutController(LayoutController):
         )
 
     def update(self, layout: Layout):
+        self.update_table()
         layout.update(self.panel)
+
+    def update_table(self):
+        shortcuts = self.formatted_shortcuts()
+        key = self.current_key
+        # color last pressed key
+        if key is not None:
+            for i, value in enumerate(shortcuts):
+                if f'<{key}>' in value:
+                    shortcuts[i] = value.replace(
+                        f'<{key}>', f'<[blue]{key}[/blue]>'
+                    )
+        self.columns = Columns(
+            shortcuts,
+            equal=True,
+            expand=True,
+        )
+        self.align.renderable = self.columns
+
+    def formatted_shortcuts(self):
+        result = []
+        re_title = re.compile(r'shortcut_name *= *(.*)$')
+        for name, func in (self._shortcuts | self.shortcuts_global).items():
+            doc = inspect.getdoc(func)
+            try:
+                title = re.search(re_title, doc).group(1)
+            except TypeError:
+                raise TypeError(
+                    f'Shortcut function "{func.__name__}" does not have '
+                    'a docstring containing shortcut_name'
+                )
+            result.append('<{}> {}'.format(name, title))
+        self.formatted = result
+        return result
 
     @property
     def codes(self):
@@ -213,11 +290,27 @@ class ShortcutsLayoutController(LayoutController):
     def codes(self, codes):
         self._codes = codes
         if codes and 33 <= codes[0] <= 126:
-            self.align.renderable = 'Key pressed : {}'.format(chr(codes[0]))
+            char = chr(codes[0])
+            if char in self.keys:
+                self.current_key = char
+                self.page.update_by_controller(self)
+
+    @property
+    def shortcuts(self):
+        return self._shortcuts
+
+    @shortcuts.setter
+    def shortcuts(self, value: dict[str, Callable]):
+        self._shortcuts = value
+        self.keys = list(
+            self._shortcuts.keys() | self.shortcuts_global.keys()
+        )
+        self.update_table()
+        self.align.renderable = self.columns
 
 
 class Page(ABC):
-    def __init__(self, loop, *args, **kwargs):
+    def __init__(self, loop: 'MainController', *args, **kwargs):
         self.controllers = {
             'header': None,
             'footer': None,
@@ -230,7 +323,11 @@ class Page(ABC):
         self.loop = loop
 
         # any page gives shortcuts info
-        self.controllers['footer'] = ShortcutsLayoutController()
+        self.controllers['footer'] = ShortcutsLayoutController(
+            loop.shortcuts_global,
+            page=self,
+        )
+
         self.init_controllers(*args, **kwargs)
 
     def update(self):
@@ -272,6 +369,7 @@ class Page(ABC):
             self.loop.shortcuts = controller.shortcuts
             self._focus_controller = controller
             controller.set_border_style('green')
+            self.controllers['footer'].shortcuts = controller.shortcuts
 
     @property
     def codes(self):
@@ -287,7 +385,9 @@ class TablePage(Page):
         info = RowLayoutController()
         self.controllers.update({
             'body': TableLayoutController(
-                self, model, info_layout_controller=info
+                model,
+                info_layout_controller=info,
+                page=self,
             ),
             'info': info,
         })
@@ -295,7 +395,8 @@ class TablePage(Page):
 
 
 class ExitLayoutController(LayoutController):
-    def __init__(self, loop: 'MainController'):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.panel_view = Panel(
             Align.center(
                 f'[red]Quitter le programme ?[/red] Oui <o> Non <n>{self.border_style}',
@@ -305,7 +406,7 @@ class ExitLayoutController(LayoutController):
             title='',
             border_style=self.border_style,
         )
-        self.loop = loop
+        self.loop = self.page.loop
 
         self.shortcuts = {
             'o': self.stop,
@@ -313,9 +414,15 @@ class ExitLayoutController(LayoutController):
         }
 
     def stop(self):
+        '''Stop send signal to stop the loop, then exit the program
+        shortcut_name = Oui
+        '''
         self.loop.exit_program = True
 
     def resume(self):
+        '''Cancel stop
+        shortcut_name = Non
+        '''
         self.loop.shortcuts = {}
         self.loop._move()
 
@@ -326,7 +433,7 @@ class ExitLayoutController(LayoutController):
 class ExitPage(Page):
     def init_controllers(self, *args, **kwargs):
         self.controllers.update({
-            'body': ExitLayoutController(self.loop),
+            'body': ExitLayoutController(page=self),
         })
         self.focus = 'body'
 
@@ -363,22 +470,29 @@ class MainController(Browser):
 
         self.exit_program = False
         self.shortcuts = {}
+        self.shortcuts_global = {
+            'w': self.go_backward,
+            'x': self.go_foreward,
+            'q': self.quit_dialog,
+        }
         super().__init__(TablePage(self, model=Player))
+
+    def quit_dialog(self):
+        '''Open quit dialog
+        shortcut_name = Quitter
+        '''
+        self._page = ExitPage(self)
+        self.need_update = True
 
     def handle_shortcuts(self):
         entries = self.kb.read()
         codes = [ord(c) for c in entries]
-        # if len(entries) == 1 and self.last_codes == codes == [27]:
-        if codes == [ord('q')]:
-            self._page = ExitPage(self)
-            self.need_update = True
 
-        # self._page.update(codes)
         if codes:
             self.last_codes = copy.deepcopy(codes)
             self._page.codes = codes
 
-        for code, func in self.shortcuts.items():
+        for code, func in (self.shortcuts | self.shortcuts_global).items():
             if isinstance(code, str):
                 code = ord(code)
             if code in codes:
